31a3916775398a13b28b52945c9b0547d715b5e8
Craig Pounds <nweaver@windstream.net>
2018-04-09 10:19:45 -0500

    2nd attempt

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,15 @@
 'use strict';
 
 const store = (function() {
-
+  let bigObject = {
+    items: [
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'bread', checked: false },
+    ],
+    hideCheckedItems: false,
+    searchTerm: '',
+  };
+  return 
 }());
