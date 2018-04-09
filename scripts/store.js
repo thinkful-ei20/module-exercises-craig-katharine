0ef9b145931992d7f2600abc894d3cfc5a35af52
Craig Pounds <nweaver@windstream.net>
2018-04-09 15:09:22 -0500

    fixed find and delete

--- scripts/store.js
+++ scripts/store.js
@@ -12,16 +12,53 @@ const store = (function() {
     ],
     hideCheckedItems: false,
     searchTerm: '',
+    findById: function(id) {
+      return items.find(e => e === id);
+    },
+    addItem: function(name) {
+      try {
+        Item.validateName(name);        
+        this.items.push(Item.create(name));
+      } catch (e) {
+        console.log('Cannot add item', e);
+      }      
+    },
+    findAndToggleChecked: function(id) {
+      this.findById(id).checked = this.findById(id).checked === true ? this.findById(id).checked = false : this.findById(id).checked = true;
+    },
+    findAndUpdateName: function(id, newName) {
+      try {
+        Item.validateName(name);
+        Item.findById(id).name = newName;
+      } catch (e) {
+        console.log('Cannot update name:', e);
+      }
+    },
+    findAndDelete: function(id) {
+      const index = items.findIndex(e => e.id === id);
+      items.splice(index, 1);
+    }
   };
   
   const localSearchTerm = store.searchTerm;
   const localHideCheckedItems = store.hideCheckedItems;
   const items = store.items;
 
+  const findById = store.findById;
+  const addItem = store.addItem;
+  const findAndToggleChecked = store.findAndToggleChecked;
+  const findAndUpdateName = store.findAndUpdateName;
+  const findAndDelete = store.findAndDelete;
+
   return {
     localSearchTerm,
     localHideCheckedItems,
     items,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
 
 }());
