d3c9337c4a4475348e69f4a9d4a42598a1e98631
Craig Pounds <nweaver@windstream.net>
2018-04-09 11:44:24 -0500

    finished item factory and validator

---
+++ scripts/Item.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Item = (function () {
+  const foo = 'bar';
+  const validateName = function(name) {    
+    if (!name) throw new TypeError('Name does not exist'); 
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      'name': name,
+      checked: false,
+    };
+  };
+
+  return {
+    validateName,
+    create,
+  };
+}());
+
